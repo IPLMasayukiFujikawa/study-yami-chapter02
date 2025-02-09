12ba231d7b4dd625a7c1c956edc365a395873ff5
Masayuki Fujikawa <ma-kappa@oak.dti.ne.jp>
2025-02-09 20:12:47 +0900

    サンプルアプリケーションの実装（天気、登場人物解析、画像解析）

--- main.py
+++ main.py
@@ -2,17 +2,15 @@
 import openai
 
 
-def main():
-    # # 環境変数からAPIキーを取得
-    # コンストラクタで指定不要なら取得不要
-    # api_key = os.getenv("OPENAI_API_KEY")
+def get_wheather(client):
+    """東京の天気を回答として返却する。
 
-    # OpenAI クライアントの作成
-    # パラメータとして指定する必要があるかどうか？
-    # OPENAI_API_KEY に正しいキーが指定されていればOKか？
-    # client = openai.OpenAI(api_key=api_key)
-    client = openai.OpenAI()
+    Args:
+        client (OpenAI): OpenAIクラスインスタンス
 
+    Returns:
+        str: 回答文字列
+    """
     # API呼び出し用のメッセージ
     # contentに質問文字列を設定する。
     messages = [
@@ -28,7 +26,116 @@ def main():
     )
 
     # 結果を表示
-    print(response.choices[0].message.content.strip())
+    return response.choices[0].message.content.strip()
+
+
+def get_charactors(client):
+    """質問の登場人物を返却する。
+
+    Args:
+        client (OpenAI): OpenAIクラスインスタンス
+
+    Returns:
+        str: 回答文字列
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": '人物一覧を次のJSON形式で出力してください。¥n{"people"}: ["aaa", "bbb¥]}',
+            },
+            {
+                "role": "user",
+                "content": "昔々あるところにおじいさんとおばあさんがいました。"
+            },
+        ],
+        response_format={"type": "json_object"},
+    )
+
+    return response.choices[0].message.content
+
+
+def get_description_of_image(client):
+    """指定画像の説明を返却する。
+
+    Args:
+        client (OpenAI): OpenAIクラスインスタンス
+
+    Returns:
+        str: 回答文字列
+    """
+    image_url = "https://as2.ftcdn.net/v2/jpg/01/11/12/43/1000_F_111124309_EDL2btGKhiqXBFW2H8tXQuqDdl4JJPyO.jpg"
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "画像を説明してください。"},
+                    {"type": "image_url", "image_url": {"url": image_url}},
+                ],
+            },
+        ],
+    )
+
+    return response.choices[0].message.content
+
+
+# def get_description_of_localimage(client):
+#     """指定画像（ローカル）の説明を返却する。
+
+#     Args:
+#         client (OpenAI): OpenAIクラスインスタンス
+
+#     Returns:
+#         str: 回答文字列
+#     """
+#     # ローカル画像ファイルのパス
+#     image_path = "/Users/m.fujikawa/Pictures/202411_ウォーキング/IMG_1137.png"
+
+#     # 画像ファイルを開いてリクエストに添付
+#     with open(image_path, "rb") as image_file:
+#         response = client.chat.completions.create(
+#             model="gpt-4o-mini",
+#             messages=[
+#                 {
+#                     "role": "user",
+#                     "content": [
+#                         {"type": "text", "text": "画像を説明してください。"},
+#                         {"type": "image_file", "image_file": image_file},
+#                     ],
+#                 },
+#             ],
+#         )
+
+#     return response.choices[0].message.content
+
+
+def main():
+    """メインエントリポイント
+    """
+    # OpenAI クライアントの作成
+    # 環境変数 OPENAI_API_KEY にキーを指定しておくこと。
+    client = openai.OpenAI()
+
+    # # 東京の天気を返却する。
+    # res_wheather = get_wheather(client)
+    # print(res_wheather)
+
+    # # 登場人物を返却する。
+    # res_charactors = get_charactors(client)
+    # print(res_charactors)
+
+    # 指定画像の説明を返却する。
+    res_desc_of_img = get_description_of_image(client)
+    print(res_desc_of_img)
+
+    # # 指定画像（ローカル）の説明を返却する。
+    # res_desc_of_localimg = get_description_of_localimage(client)
+    # print(res_desc_of_localimg)
 
 
 if __name__ == '__main__':
